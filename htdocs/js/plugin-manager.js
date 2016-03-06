b7758ab7a171ef5987eab84a48ca264bc17d82e9
Villena Guillaume <villenagui.free.fr@gmail.com>
2016-03-06 14:58:26 +0100

    pluginManager update

--- htdocs/js/plugin-manager.js
+++ htdocs/js/plugin-manager.js
@@ -24,7 +24,7 @@ Plugins.prototype.loadPlg = function()
     for ( var item in obj){
         console.log(item, obj[item]);
 
-        xhr.open("GET", 'assets/plugins/'+obj[item], false);
+        xhr.open("GET", 'assets/plugins/'+item +'/'+obj[item], false);
         xhr.send(null);
         if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)){
             throw new Error("Impossible de charger le fichier  (code HTTP : " + xhr.status + ").");
@@ -39,6 +39,10 @@ Plugins.prototype.loadPlg = function()
 
 Plugins.prototype.add = function (plg){
     this.pluginList.push(plg);
+    if (plg.externalJs.length > 0)
+    {
+        this.loadExternalJs(plg.externalJs);
+    }
     console.log("hey ");
     var li = document.createElement('li');
     li.id = plg.name;
@@ -55,6 +59,34 @@ Plugins.prototype.add = function (plg){
     li.appendChild(a);
 
     document.getElementById("plg-drop").appendChild(li);
+};
+
+Plugins.prototype.loadExternalJs = function (array) {
+
+    for (i=0;i<array.length;i++)
+    {
+        var jsData = document.createElement('script');
+        jsData.src = array[i];
+        document.body.appendChild(jsData);
+    }
+
+
+};
+
+Plugins.prototype.createCORSRequest = function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
 };
 
 Plugins.prototype.rowByName = function (name) {
