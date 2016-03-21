4044c210eda0a7447c69d5a8db9bcd4a91e55648
Guillaume Villena <guiguivil@gmail.com>
2016-03-21 23:20:07 +0100

    Implements Lang.js class

--- htdocs/js/plugin-manager.js
+++ htdocs/js/plugin-manager.js
@@ -3,44 +3,43 @@
  */
 
 
-Plugins = function()
-{
+Plugins = function () {
     this.pluginList = [];
 };
 
-Plugins.prototype.loadPlg = function()
-{
-    var xhr =  (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+Plugins.prototype.loadPlg = function () {
+    var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 
 
     xhr.open("GET", 'assets/plugins/main.json', false);
     xhr.send(null);
-    if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)){
+    if (xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) {
         throw new Error("Impossible de charger le fichier  (code HTTP : " + xhr.status + ").");
     } // Code == 0 en local
 
     var obj = JSON.parse(xhr.responseText);
 
-    for ( var item in obj){
+    for (var item in obj) {
         console.log(item, obj[item]);
 
         var jsData = document.createElement('script');
-        jsData.src = 'assets/plugins/'+item +'/'+obj[item];
+        jsData.src = 'assets/plugins/' + item + '/' + obj[item];
         document.body.appendChild(jsData);
 
     }
 };
 
-Plugins.prototype.add = function (plg){
+Plugins.prototype.add = function (plg) {
     this.pluginList.push(plg);
-    if (plg.externalJs.length > 0)
-    {
+
+    if (plg.externalJs) {
         this.loadExternalJs(plg.externalJs);
     }
+
+
     console.log("hey ");
 
-    if (plg.ui != "")
-    {
+    if (plg.ui != "") {
         var li = document.createElement('li');
         li.id = plg.name;
         li.onclick = function () {
@@ -63,8 +62,7 @@ Plugins.prototype.add = function (plg){
 
 Plugins.prototype.loadExternalJs = function (array) {
 
-    for (i=0;i<array.length;i++)
-    {
+    for (i = 0; i < array.length; i++) {
         var jsData = document.createElement('script');
         jsData.src = array[i];
         document.body.appendChild(jsData);
@@ -75,8 +73,7 @@ Plugins.prototype.loadExternalJs = function (array) {
 
 
 Plugins.prototype.rowByName = function (name) {
-    for (i=0;i<this.pluginList.length;i++)
-    {
+    for (i = 0; i < this.pluginList.length; i++) {
         //console.log(this.pluginList[i]);
         if (name == this.pluginList[i].name)
             return this.pluginList[i];
